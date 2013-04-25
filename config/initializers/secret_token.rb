af0c876181f2d2353f1de1b7a997fe8f540d81e7
Paul Simpson-Parry <paulsimpsonparry@gmail.com>
2013-04-24 22:43:17 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fourtech::Application.config.secret_token = 'c65761ac33ddceaa5ab613fb5225c8985a3609d4a8c97db7e8dccc6b056b19ec178458a45af3ec5a4104a9b8e9957ee429d0094b332b6eb2abd651af7ea9b0e6'
